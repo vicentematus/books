277bca6337e8dcd968ddb02e171c6407910c64f6
Vicente Matus <38754555+vicentematus@users.noreply.github.com>
2025-01-19 18:10:05 -0300

    feat: index in wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,65 @@
 <script lang="ts">
-    let data =  $props();
+	import { onMount } from 'svelte';
+	let data = $props();
+	const {
+		data: { books }
+	} = data;
 
-    const {data: {books}} = data
+	let container: HTMLElement;
 
-    // console.log({books})
+	const width = $derived(books.length * 300);
+	$inspect('cuanto sacamos', width);
 </script>
 
+<div class="flex min-h-screen w-full items-center justify-center">
+	<div class="min-h-screen min-w-[100vw]">
+		<h1 class="text-center font-serif text-3xl font-semibold">Que estoy leyendo</h1>
+		<section class="scrollbar-visible mt-2 flex gap-4 overflow-x-auto pb-4">
+			{#each books as book}
+				<div class="h-full w-[300px] flex-shrink-0 rounded-md border border-gray-500 shadow-2xl">
+					<div class="p-4">
+						<h1 class="text-center text-2xl font-bold">{book.title}</h1>
+						<p class="text-center">{book.author}</p>
+					</div>
 
-{#each books as book}
-    <div>
-        {JSON.stringify(book.imageUrl)}
-        <img src={book.imageUrl} alt={book.title} />
-        <h1>{book.title}</h1>
-        <p>{book.author}</p>
-    </div>
-{/each}
+					{#if book.imageUrl}
+						<img src={book.imageUrl} alt={book.title} class="mx-auto h-full w-full object-cover" />
+					{/if}
+				</div>
+			{/each}
+		</section>
+	</div>
+</div>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+	.scrollbar-visible {
+		scrollbar-gutter: stable;
+		-webkit-overflow-scrolling: touch;
+		&::-webkit-scrollbar {
+			height: 8px;
+			background: #f1f1f1;
+		}
+		&::-webkit-scrollbar-thumb {
+			background: #888;
+			border-radius: 4px;
+		}
+		scrollbar-width: thin;
+	}
+
+	.animate-scroll {
+		animation: scroll 15s linear infinite;
+	}
+
+	@keyframes scroll {
+		0% {
+			transform: translateX(0);
+		}
+		100% {
+			transform: translateX(-35%);
+		}
+	}
+
+	.animate-scroll:active {
+		animation-play-state: paused;
+	}
+</style>
