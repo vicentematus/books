25997d6710241711fd948c57a22aa789c44c4558
Vicente Matus <38754555+vicentematus@users.noreply.github.com>
2025-01-19 23:09:41 -0300

    feat: tailwind config for random fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,23 @@
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				times: ['Times New Roman', ...defaultTheme.fontFamily.serif],
+				garamond: ['Garamond', ...defaultTheme.fontFamily.serif]
+				// TODO: Check if I should add random fonts on the title.
+				// trebuchet: ['Trebuchet MS', ...defaultTheme.fontFamily.sans],
+				// brushscript: ['Brush Script MT', ...defaultTheme.fontFamily.serif],
+				// lora: ['Lora Variable', ...defaultTheme.fontFamily.serif],
+				// inknut: ['Inknut Antiqua', ...defaultTheme.fontFamily.serif],
+				// arvo: ['Arvo', ...defaultTheme.fontFamily.serif]
+			}
+		}
 	},
 
 	plugins: [typography]
